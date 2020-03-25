4ca97626959d5f623f05cb96f9b4d0f51807d991
maciej.koziara <maciej.koziara@airhelp.com>
2020-03-25 20:52:51 +0100

    Init commit

---
+++ src/main/java/pl/infoshare/webinar/InfoshareWebinarSecurityApplication.java
@@ -0,0 +1,13 @@
+package pl.infoshare.webinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoshareWebinarSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoshareWebinarSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/infoshare/webinar/InfoshareWebinarSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.infoshare.webinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoshareWebinarSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
